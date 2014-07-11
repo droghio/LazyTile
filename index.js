6fd742f8def7212e2a8c1dae48f4242dc8256ee4
John Drogo <admin@jldapps.com>
2014-07-11 09:26:30 -0400

    Waypoint configuration.

--- index.js
+++ index.js
@@ -16,6 +16,13 @@
 // July 10, 2014
 // John Drogo
 
+/* Notes.
+
+   Current implementation will not be able to resize well, there will be a massive remap overhead.
+   Flex boxes would be nice, but I'm not sure they are cross-browser compliant enough.
+
+*/
+
 
 images = [
 
@@ -35,31 +42,36 @@ images = [
 
 function addImages(){
     
-    if (!images.length){ $.waypoints("destroy"); return; }
-    collagerow = $("<div class='collagerow'></div><br />")
-    collagecontainer.append(collagerow)
+    //Add row containers.
+    for (var row = 0; row < rowstoadd; row++){
 
-    for (index = 0; index < numbercolumns; index++){
-        if (!images.length){ $.waypoints("destroy"); break; }
-
-        image = images.pop()
-        collagerow.append(
-            "<div class='collageitem' style='background-mage: url(\"" + image.src + "\")'>" +
-                "<p class='collagedescription'>" + image.desc + "</p>" +
-            "</div>"
-        )
+        //Make a row container.
+        if (!images.length){ $.waypoints("destroy"); return; } //Nothing left, clean up.
+        collagerow = $("<div class='collagerow'></div><br />")
+        collagecontainer.append(collagerow)
+    
+        //Add items to the row.
+        for (var index = 0; index < numbercolumns; index++){
+            if (!images.length){ $.waypoints("destroy"); break; } //Nothing left, clean up.
+    
+            image = images.pop()
+            collagerow.append(
+                "<div class='collageitem' style='background-mage: url(\"" + image.src + "\")'>" +
+                    "<p class='collagedescription'>" + image.desc + "</p>" +
+                "</div>"
+            )
+        }
+    
     }
 
 
-   /*$.waypoints("destroy");
+   //Waypoints does not seem to update its firing point when the element resizes, so we must do this manually.
+   $.waypoints("destroy");
     collagecontainer.waypoint(function() {
         addImages()
     },{
-        offset: function() {
-            return (window.innerHeight)+(Number($(this).css("height").substr(0, $(this).css("height").length-2)))
-            //(-1*Number($(this).css("height").substr(0, $(this).css("height").length-2)))
-        }
-    });*/
+        offset: "bottom-in-view" 
+    });
 
 }
 
@@ -67,16 +79,14 @@ function addImages(){
 //Waypoint triggers when top of the element hits the top of the viewport, use offsets to bring this down.
 $(document).ready(function (){
 
-    numbercolumns = 5
+    numbercolumns = 1
+    rowstoadd = 3 
     collagecontainer = $("#collagecontainer")
 
     collagecontainer.waypoint(function() {
         addImages()
     },{
-        offset: "bottom-in-view"
-        //function() {
-            //return (window.innerHeight)+(Number($(this).css("height").substr(0, $(this).css("height").length-2)))
-            //(-1*Number($(this).css("height").substr(0, $(this).css("height").length-2)))
-        //}
+        offset: function(){ $.waypoints('viewportHeight')-$(this).height() },
+        triggerOnce: true
     });        
 })
